eeb345864b2274d84fec15e8cf8d938c474d32e3
Mohnish <mohnish.a007@gmail.com>
2023-06-12 19:37:07 +0100

    The  input component setup but need modification

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-
-
+import "./App.css";
+import InputField from './components/inputField';
 
 const App: React.FC = () => {
   return (
+    <>
     <div className="App">
-      Hello World!!! 
+     <span className='heading'> TO DO's</span>
+      <InputField />  
+      {/* The letter must start with capital. */}
     </div>
+    </>
   );
 }
 
---
+++ react-ts/src/components/inputField.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+const InputField =() => {
+  return (
+    <form className='input'>
+        <input type="input" placeholder="My To Do's " className='input_box'/>
+        <button className='input_submit' type='submit'> ADD </button>
+    </form>
+  )
+}
+
+export default InputField
