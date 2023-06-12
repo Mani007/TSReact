667667944c7f2e44ffd33ef294f396edbde94cbd
Mohnish <mohnish.a007@gmail.com>
2023-06-12 23:33:39 +0100

    The  input component setup working fine

--- react-ts/src/components/inputField.tsx
+++ react-ts/src/components/inputField.tsx
@@ -3,7 +3,7 @@ import './style.css'
 const InputField =() => {
   return (
     <form className='input'>
-        <input type="input" placeholder="My To Do's " className='input_box'/>
+        <input type="input" placeholder="My To Do's " className='input__box'/>
         <button className='input_submit' type='submit'> ADD </button>
     </form>
   )
