a0bf2f58037a1612969cbc1a116c9ebeb90684c2
Mohnish <mohnish.a007@gmail.com>
2023-06-13 15:16:18 +0100

    The useRef hook in type script applied

--- react-ts/src/components/inputField.tsx
+++ react-ts/src/components/inputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './style.css'
 interface Props{
   todo: string,
@@ -7,8 +7,12 @@ interface Props{
 }
 
 const InputField: React.FC<Props> =({todo, setTodo, handleAdd}) => {
+const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <form className='input' onSubmit={handleAdd}>
+    <form className='input' onSubmit={(e) =>{
+        handleAdd(e)
+        inputRef.current?.blur()
+        }}>
         <input type="input" 
         value={todo}
         onChange={
