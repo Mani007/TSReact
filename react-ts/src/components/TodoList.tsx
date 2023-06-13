0d532c7d823c3677da58bb4a567ae40277b858c6
Mohnish <mohnish.a007@gmail.com>
2023-06-13 20:43:18 +0100

    Single todos component with icon

--- react-ts/src/components/TodoList.tsx
+++ react-ts/src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Todo } from '../model'
 import "./style.css"
+import SingleTodo from './SingleTodo'
 
 interface Props {
     todos: Todo[],
@@ -9,7 +10,15 @@ interface Props {
 const TodoList: React.FC<Props> = ({todos, setTodos}) => {
   return (
     <div className='todos'>
-        {todos.map((todo) => (<li>{todo.todo}</li>))}
+        {todos.map((todo) => (
+        // <li>{todo.todo}</li>
+        <SingleTodo
+        todo = {todo}
+        key = {todo.id}
+        todos = {todos}
+        setTodos = {setTodos}
+        />
+        ))}
         </div>
   )
 }
